6fe14ff47accb6cabdbbd9cc919a5c46acc53074
JoyontaSaha <Joyontasaha8@gmail.com>
2019-10-21 01:34:54 +0600

    Add files via upload

---
+++ eclipse_in_5_steps/src/eclipse_in_5_steps/HelloWorld.java
@@ -0,0 +1,12 @@
+package eclipse_in_5_steps;
+
+public class HelloWorld {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		
+		
+	}
+}
---
+++ first-web-application/src/main/java/com/in28mintues/first_web_application/App.java
@@ -0,0 +1,13 @@
+package com.in28mintues.first_web_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
